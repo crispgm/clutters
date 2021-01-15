b21470c2c609076e692ce98ee7ae233938b7e5a6
David Zhang <crispgm@gmail.com>
2021-01-15 15:41:43 +0800

    fix: path

--- zf-notifier/check.sh
+++ zf-notifier/check.sh
@@ -3,9 +3,12 @@
 FILEPATH="./temp/zf"
 TEMP_PID="./temp/zfpid"
 ALL_PID="./temp/zfallpid"
-WEBHOOK="https://open.feishu.cn/open-apis/bot/v2/hook/97d4693e-a5e0-4684-a7ab-e7d4b3b87f35"
+WEBHOOK="https://open.feishu.cn/open-apis/bot/v2/hook/117bd557-dad0-4cf2-ba1b-81e1e002de33"
+# WEBHOOK="https://open.feishu.cn/open-apis/bot/v2/hook/97d4693e-a5e0-4684-a7ab-e7d4b3b87f35"
 
-# wget -O $FILEPATH "https://www.zfrontier.com/shop/cate/140,25"
+export PATH="/usr/local/bin:$PATH"
+
+wget -O $FILEPATH "https://www.zfrontier.com/shop/cate/140,25"
 
 if [ ! -f "$FILEPATH" ]; then
     exit 1
@@ -31,5 +34,5 @@ do
         $WEBHOOK \
         -H 'Content-Type: application/json' \
         -d "{\"msg_type\": \"text\",\"content\": {\"text\": \"发现ZF键帽新团购 $NAME https://www.zfrontier.com/app$HREF\"}}"
+    echo $PID >> $ALL_PID
 done
-echo $DIFF_PID | awk 'BEGIN{RS=" "; ORS="\n"}' >> ./temp/zfallpid
